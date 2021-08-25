aa7b35baac469792c092bbf334e29eeeee8f8b06
Michael Guidetti <mguidetti@users.noreply.github.com>
2021-08-25 09:31:31 -0400

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports =
+  process.env.NODE_ENV === 'test'
+    ? {
+        presets: ['@babel/env'],
+        plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime'],
+      }
+    : {}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Sortable from './sortable'
+
+export { Sortable }
